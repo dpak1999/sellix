1dfef739f17102f855be8e48b2591a56dad55747
deepak <deepakkumardash313@gmail.com>
2025-09-26 04:21:03 +0530

    Chore : update font and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>hellow work</h2>
-      <Button>test</Button>
+      <Button variant={"elevated"}>test</Button>
     </div>
   );
 }
