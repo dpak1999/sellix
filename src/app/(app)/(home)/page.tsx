5b9a671a7e4e5587508b9a29adadcb183453effd
deepak <deepakkumardash313@gmail.com>
2025-09-26 15:46:09 +0530

    add : payload cms

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { getPayload } from "payload";
+import configPromise from "@payload-config";
 
-export default function Home() {
-  return (
-    <div>
-      <Button variant={"elevated"}>test</Button>
-    </div>
-  );
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({ collection: "categories" });
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
