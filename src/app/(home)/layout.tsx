ce6a1d6e00b8edee7eac674f8dd2248cacadf444
deepak <deepakkumardash313@gmail.com>
2025-09-26 14:58:13 +0530

    Add : footer and checkbox component

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 
 import { Navbar } from "./_components/navbar";
+import { Footer } from "./_components/footer";
 
 export default function HomeLaout({
   children,
@@ -8,9 +9,10 @@ export default function HomeLaout({
   children: ReactNode;
 }>) {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      {children}
+      <div className="flex-1 bg-[#f4f4f0]">{children}</div>
+      <Footer />
     </div>
   );
 }
