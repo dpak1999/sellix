415433f35324f125ad0d6e3f99a0b4db82eeff34
deepak <deepakkumardash313@gmail.com>
2025-10-01 02:31:20 +0530

    Add : category and category dropdown

---
+++ src/app/(app)/(home)/_components/search-filters/sub-category-menu.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+import { Category } from "@/payload-types";
+
+interface SubCategoryMenuProps {
+  category: Category;
+  isOpen: boolean;
+  position: { top: number; left: number };
+}
+
+export const SubCategoryMenu = ({
+  category,
+  isOpen,
+  position,
+}: SubCategoryMenuProps) => {
+  if (
+    !isOpen ||
+    !category.subcategories ||
+    category.subcategories.length === 0
+  ) {
+    return null;
+  }
+
+  const backgroundColor = category.color ?? "#F5F5F5";
+
+  return (
+    <div
+      className="fixed z-100"
+      style={{ top: position.top, left: position.left }}
+    >
+      <div className="w-60 h-3" />
+
+      <div
+        style={{ backgroundColor }}
+        className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
+      >
+        <div>
+          {category.subcategories.map((subcat: Category) => (
+            <Link
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
+              href={"/"}
+              key={subcat.slug}
+            >
+              {subcat.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
